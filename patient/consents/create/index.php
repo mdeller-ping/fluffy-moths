ecf52b21989133c5352c7fc97fb6699f17e11958
mdeller-ping <mdeller@pingidentity.com>
2020-06-09 13:56:17 -0400

    expiration

--- patient/consents/create/index.php
+++ patient/consents/create/index.php
@@ -67,6 +67,13 @@
       </label>
    </div>
 
+   <div class="form-group row">
+    <label for="inputExpiration" class="col-sm-2 col-form-label">Expiration Date</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control" id="inputExpiration" placeholder="Expiration Date">
+    </div>
+  </div>
+
   </div>
 
   <!-- footer -->
