4cc61c14a7dd7c6d2f69bdff9ec20d3791f5eca6
mdeller-ping <mdeller@pingidentity.com>
2020-06-09 13:01:52 -0400

    revoke

--- patient/consents/inspect/index.php
+++ patient/consents/inspect/index.php
@@ -40,13 +40,11 @@
         </li>
         <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
+          Actions
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+          <a class="dropdown-item" href="revoke/?consent=<?php echo $consentId ?>">Revoke</a>
+          <a class="dropdown-item" href="#">Enable</a>
         </div>
       </li>
       </ul>
